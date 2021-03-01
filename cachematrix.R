9cee087ce147e43b554250bc8452cce5c2178b50
bkaww <79866313+bkaww@users.noreply.github.com>
2021-03-02 00:18:09 +0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,8 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {inv <- NULL
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
   set <- function(y) {
     x <<- y
     inv <<- NULL
