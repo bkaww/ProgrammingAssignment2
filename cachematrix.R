9e78811b2a488dcb3f6d1d543019740101a2a887
bkaww <79866313+bkaww@users.noreply.github.com>
2021-03-02 00:20:28 +0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,29 +4,53 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
-  }
-  get <- function() x
-  setinv <- function(inverse) inv <<- inverse
-  getinv <- function() inv
-  list(set = set, get = get, setinv = setinv, getinv = getinv)
-}
 
+}
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-     inv <- x$getinv()
-  if(!is.null(inv)) {
-    message("getting cached result")
-    return(inv)
-  }
-  data <- x$get()
-  inv <- solve(data, ...)
-  x$setinv(inv)
-  inv
+        ## Return a matrix that is the inverse of 'x'
+}
+  
+## The inverse of a Matrix
+## It always time-wasting to inverse a matrix and there may be some advantages 
+##to program this action, because it will save your time
+## These functions are used to create a special "matrix" object and computes the inverse of the special "matrix"
+
+## makeCacheMatrix: This function creates a special “matrix” object that can cache its inverse
+
+  makeCacheMatrix <- function(x = matrix()) {
+    
+   r <- NULL
+   set <- function(y) {
+       
+             x <<- y
+             r <<- NULL
+    }
+  get <- function() x
+  setsolve <- function(solve) r <<- solve
+  getsolve <- function() r
+  list ( set = set, get = get, setsolve=setsolve, getsolve=getsolve)
+
+}
+
+
+## cacheSolve: This function computes the inverse of the special “matrix” returned by makeCacheMatrix above. 
+##If the inverse has already been calculated (and the matrix has not changed), 
+##then the cacheSolve should retrieve the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+    r <- x$getsolve()
+    if(!is.null(r)) {
+        message("getting cached data")
+        return(r)
+    }
+    
+    data <- x$get()
+    r <- solve(data,...)
+    x$setsolve(r)
+    r
 }
